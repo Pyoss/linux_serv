cf0e34d2de76231fad7a382d6a2685ed54795f0d
Pyoss <yisgit@gmail.com>
2019-05-26 16:48:08 +0300

    Update bot_watcher.py

--- bot_watcher.py
+++ bot_watcher.py
@@ -3,5 +3,6 @@ import telebot
 
 bot = telebot.TeleBot('777849028:AAFKdy8OJcLn37H7A8bJVsSCTSB-5S37zf4')
 admin_id = 197216910
-print('sending')
+print(sys.argv[-1])
+
 bot.send_message(admin_id, sys.argv[-1])
