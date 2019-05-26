481df16e4a790d9e429185be8e17a26cd531bfb1
Pyoss <yisgit@gmail.com>
2019-05-27 00:37:20 +0300

    Update test.py

--- test.py
+++ test.py
@@ -14,11 +14,11 @@ bot.send_message(admin_id, 'Инициация бота...')
 
 @bot.message_handler(commands=['restart'])
 def start(message):
-  subprocess.call(['./update.sh'])
+  os.execl(sys.executable, 'python',  __file__, *sys.argv[1:])
   
 @bot.message_handler(commands=['update'])
 def start(message):
-  os.execl(sys.executable, 'python',  __file__, *sys.argv[1:])
+  subprocess.call(['./update.sh'])
 
 @bot.message_handler(commands=['error'])
 def start(message):
@@ -27,18 +27,6 @@ def start(message):
 @bot.message_handler(commands=['stop'])
 def start(message):
     bot.stop_polling()
-
-
-@bot.message_handler(commands=['update'])
-def start(message):
-    main.send_error(repr(message))
-    try:
-        payload = message.successful_payment.invoice_payload
-        total_amount = message.successful_payment.total_amount
-        message_id = message.message_id
-        menu_messages.payment_handle(message.chat.id, payload, total_amount, message_id)
-    except Exception as e:
-        main.send_error(repr(e))
 
 
 @bot.message_handler(commands=['echo'])
