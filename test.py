6ad7725b8546ecb25ff9c798354e136c5c4d7155
Pyoss <yisgit@gmail.com>
2019-05-26 16:32:25 +0300

    Update test.py

--- test.py
+++ test.py
@@ -7,6 +7,7 @@ print(True)
 time.sleep(5)
 
 print(True)
+raise Exeption('test')
 
 #subprocess.call(['./monitor.sh']) # Thanks @Jim Dennis for suggesting the []
 #os.execl(sys.executable, 'python',  __file__, *sys.argv[1:])
