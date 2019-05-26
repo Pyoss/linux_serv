6de5f18f0cde5fa5e01cd5afd1564e0f58d07792
Pyoss <yisgit@gmail.com>
2019-05-26 23:45:37 +0300

    Update bot_watcher.py

--- bot_watcher.py
+++ bot_watcher.py
@@ -7,3 +7,9 @@ admin_id = 197216910
 print(bot.send_message(admin_id, 2))
 print(os.environ['ERROR_MSG'])
 
+
+
+if __name__ == '__main__':
+    bot.skip_pending = True
+    bot.delete_webhook()
+    bot.polling(none_stop=True)
