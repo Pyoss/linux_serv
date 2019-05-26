dbabff72ecfeba3c2f50a2835ac475fe309d5d5a
Pyoss <yisgit@gmail.com>
2019-05-26 16:44:47 +0300

    Update monitor.sh

--- monitor.sh
+++ monitor.sh
@@ -1,7 +1,7 @@
 
 #!/bin/bash
 until python -m test; do
-    $ERROR_MSG = "'myscript.py' crashed with exit code $?. Restarting..."
+    $ERROR_MSG="'myscript.py' crashed with exit code $?. Restarting..."
     python -m bot_watcher $ERROR_MSG
     sleep 1
 done
