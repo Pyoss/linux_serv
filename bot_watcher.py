0bdbe0d888d0d3df0e268c9ffa8bd493695fa38d
Pyoss <yisgit@gmail.com>
2019-05-26 23:48:06 +0300

    Update bot_watcher.py

--- bot_watcher.py
+++ bot_watcher.py
@@ -4,12 +4,4 @@ import os
 
 bot = telebot.TeleBot('777849028:AAFKdy8OJcLn37H7A8bJVsSCTSB-5S37zf4')
 admin_id = 197216910
-print(bot.send_message(admin_id, 2))
-print(os.environ['ERROR_MSG'])
-
-
-
-if __name__ == '__main__':
-    bot.skip_pending = True
-    bot.delete_webhook()
-    bot.polling(none_stop=True)
+bot.send_message(admin_id, os.environ['ERROR_MSG'])
